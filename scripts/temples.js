fe3b74fc2de85772d27c6ca9bdc7ab0cc72f3786
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-09-24 17:07:27 -0500

    added a hamburger menu

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector("#menu");
+const navLinks = document.querySelector(".navbar ul");
+
+hamButton.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+});
