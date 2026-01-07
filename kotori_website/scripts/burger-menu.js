61eb45c4f10b9b3b264b15e4e06b9607a104677d
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-07 14:17:38 -0600

    Cleared out my wdd131 folder for a Fresh start

--- kotori_website/scripts/burger-menu.js
+++
@@ -1,9 +0,0 @@
-//---Hamburger menu-------------------------------//
-  const hamButton = document.querySelector("#menu");
-  const navLinks = document.querySelector(".navbar ul");
-
-  hamButton.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-
-// -------------------------------------//
-});
--- scripts/temples.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector("#menu");
-const navLinks = document.querySelector(".navbar ul");
-
-hamButton.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-
-});
