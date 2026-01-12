d5020a9fbb64838a704cf082c059a3197e4607c0
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-12 09:06:02 -0600

    updated index and made a folder for the Final Project

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear(); // Takes the programer's computer date
+document.getElementById("currentyear").innerHTML = document.lastModififed;  // deploys the programer's computer date for copy right year
+
+document.getElementById("lastModified").innerHTML = document.lastModified; // Last Modifed Tracker
