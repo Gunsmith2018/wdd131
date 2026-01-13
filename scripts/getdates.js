5598cde6c967d81cbd49239fd544135b79283c76
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-13 17:00:19 -0600

    edited css to be neater, edited java script for getdates to work properly, edited html footer merging the two p tags into one. Centered footer at the bottom of the screen

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-const currentYear = new Date().getFullYear(); // Takes the programer's computer date
-document.getElementById("currentyear").innerHTML = document.lastModififed;  // deploys the programer's computer date for copy right year
+const currentyear = new Date().getFullYear(); // Takes the programer's computer date
+document.getElementById("currentyear").innerHTML = '©️${currentyear}';  // deploys the programer's computer date for copy right year
 
 document.getElementById("lastModified").innerHTML = document.lastModified; // Last Modifed Tracker
