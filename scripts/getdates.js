d2c8c3d26e47b75a8ac201d7debe7210253ac89c
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-14 16:42:36 -0600

    removed console.log from java script

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,5 @@
 const currentYear = new Date().getFullYear(); // Takes the programer's computer date
 document.getElementById("currentyear").innerHTML = `©️${currentYear}`;  // Copy right year
-console.log(currentYear);
+
 
 document.getElementById("lastModified").innerHTML = document.lastModified; // Last Modifed Tracker
-console.log(lasModified);
