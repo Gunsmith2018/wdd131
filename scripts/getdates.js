89f1716c4a8fe8b68f81f93cc925c28cb7da436f
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-13 19:02:26 -0600

    Added a missing defer for script plug in

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,6 @@
 const currentYear = new Date().getFullYear(); // Takes the programer's computer date
-document.getElementById("currentyear").innerHTML = `©️${currentYear}`;  // deploys the programer's computer date for copy right year
+document.getElementById("currentyear").innerHTML = `©️${currentYear}`;  // Copy right year
+console.log(currentYear);
 
 document.getElementById("lastModified").innerHTML = document.lastModified; // Last Modifed Tracker
+console.log(lasModified);
