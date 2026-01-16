13fa8361652bb049649e9c2dd55278232e6c2f88
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-15 20:23:36 -0600

    built week02 bom button and file

---
+++ week02/scripts/bom.js
@@ -0,0 +1,19 @@
+
+
+button.addEventListener('click'() => {
+    if (input.value = '') {
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('button');
+    const list = document.querySelector('____________'); // THis will link to an HTML tag
+
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = inputvalue;
+
+    deleteButton.textcontent = '❌';
+
+    li.append(deleteButton);
+    list.append(li);
+    }
+}
