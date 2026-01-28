dcb16fb069afdb1c42e1bea94496939d582af7e6
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-01-27 18:39:52 -0600

    built css for large view and mobile view

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const currentYear = new Date().getFullYear(); // Takes the programer's computer 
 document.getElementById("currentyear").innerHTML = `©️${currentYear}`;  // Copy right year
 
 
-document.getElementById("lastModified").innerHTML = document.lastModified; // Last Modifed Tracker
+document.getElementById("lastModified" ).innerHTML = document.lastModified; // Last Modifed Tracker
