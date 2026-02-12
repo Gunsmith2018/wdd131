6d4a4dc8e42ce05dc83caceec7be44bc4b564a00
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-02-12 15:19:34 -0600

    building video archive

---
+++ project/scripts/getdates.js
@@ -0,0 +1,4 @@
+
+const currentYear = new Date().getFullYear(); // Takes the programer's computer date
+document.getElementById("currentyear").innerHTML = `©️${currentYear}`;  // Copy right year
+document.getElementById("lastModified" ).innerHTML = document.lastModified; // Last Modifed Tracker
